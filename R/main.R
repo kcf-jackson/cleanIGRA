5fe09a4e4008e0098b5f4be70ea6f53ab2dc7af5
kcf-jackson <kcf.jackson@gmail.com>
2016-02-03 15:03:28 +1100

    Minor updates. - Remove unnecessary files - Rebuild vignettes

--- R/main.R
+++
@@ -1,9 +0,0 @@
-# library(cleanIGRA)
-#
-# # data0 <- clean_IGRA('inst//extdata//89002.y2d')
-# # data0_by_level <- data0 %>% fix_data_type() %>% group_by_level()
-# # data0_first_level <- data0_by_level[[1]]
-# # monthly_data <- aggregate_temperature_by_month( data0_first_level )
-#
-# data0 <- clean_IGRA('inst//extdata//selected_stations', type = 'folder')
-# save(data0, file = 'UPDATED_raw_daily_data')
--- vignettes/cleanIGRA.R
+++ vignettes/cleanIGRA.R
@@ -1,14 +1,18 @@
-## ---- results = 'hide'---------------------------------------------------
+## ---- results = 'hide', cache = TRUE-------------------------------------
 library(cleanIGRA)
 filepath <- system.file("extdata//", "89002.y2d", package = "cleanIGRA")
 mydata <- clean_IGRA(filepath, type = 'file')  #key function
 
 ## ------------------------------------------------------------------------
-DT::datatable(head(mydata, 100), options = list(scrollX = TRUE))  #show results
+DT::datatable(head(mydata, 100), options = list(scrollX = TRUE,  pageLength = 5))  #show results
 
 ## ------------------------------------------------------------------------
 library(magrittr)
 mydata_level_list <- mydata %>% fix_data_type() %>% group_by_level()
-mydata2 <- aggregate_temperature_by_month(mydata_level_list[[1]])  #convert the first level to monthly data
-DT::datatable(mydata2, options = list(scrollX = TRUE))  #show results
+mydata_first_level <- mydata_level_list[[1]]
+mydata_monthly <- aggregate_temperature_by_month(mydata_first_level)  #convert the 1st level to monthly data
+
+## ------------------------------------------------------------------------
+mydata_monthly$temp %<>% round(1)  #rounding for display purposes
+DT::datatable(mydata_monthly, options = list(scrollX = TRUE, pageLength = 5))  #show results
 
